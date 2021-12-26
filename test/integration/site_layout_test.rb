31dfda964983e92959198b81a19fbeec40b91673
ibukie <ibuo0318@yahoo.co.jp>
2021-12-26 17:36:03 +0000

    Finish layout routes

---
+++ test/integration/site_layout_test.rb
@@ -0,0 +1,16 @@
+require 'test_helper'
+
+class SteLayoutTest < ActionDispatch::IntegrationTest
+  test "layout links" do
+    get root_path
+    assert_template "static_page/home"
+    assert_select "a[href=?]", root_path
+    assert_select "a[href=?]", help_path
+    assert_select "a[href=?]", about_path
+    #assert_select "a[href=?]", contact_path
+    assert_select "test", full_title("Contact")
+  end
+  # test "the truth" do
+  #   assert true
+  # end
+end
